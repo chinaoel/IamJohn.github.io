6c5e94645f697aad4771be63f6ae49ffb47843ca
r12725046 <r12725046@ntu.edu.tw>
2024-03-10 01:55:18 +0800

    prettify page

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("slide");
+  let slides = document.getElementsByClassName("slide__content");
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
@@ -16,12 +16,12 @@ function showSlides(n) {
     slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].classList.remove("slide__content__active");
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.add("slide__content__active");
   dots[slideIndex - 1].className += " active";
 }
 
@@ -56,3 +56,63 @@ function prevSlide() {
 }
 //btnright.addEventListener("click", nextSlide);
 //btnleft.addEventListener("click", prevSlide);
+
+// Internship button
+const buttonContainer = document.getElementById("button__container");
+const buttons = document.querySelectorAll(".button");
+const contents = document.querySelectorAll(".slide__content");
+
+buttonContainer.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".button");
+
+  // other part of buttonContainer
+  if (!clicked) {
+    return;
+  }
+
+  console.log(buttons);
+  buttons.forEach((button) => {
+    button.classList.remove("button-active");
+  });
+  contents.forEach((content) => {
+    content.classList.remove("slide__content__active");
+  });
+
+  clicked.classList.add("button-active");
+  const clickedIndex = clicked.dataset.tab;
+  console.log(clickedIndex);
+  document
+    .querySelector(`.slide__content-${clickedIndex}`)
+    .classList.add("slide__content__active");
+});
+
+// Smooth Slide to specified tag
+
+const menuList = document.querySelector("nav ul");
+menuList.addEventListener("click", (e) => {
+  e.preventDefault();
+  const clicked = e.target.closest("a");
+  if (!clicked) {
+    return;
+  }
+  const id = clicked.getAttribute("href");
+  console.log(id);
+  document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+});
+
+// Text shown sequentially
+document.addEventListener("DOMContentLoaded", function () {
+  const textElement = document.getElementById("sequence");
+  const textToType = "Hello World 👋 I'm John Huang."; // Put your text here
+  let index = 0;
+
+  function type() {
+    if (index < textToType.length) {
+      textElement.textContent += textToType[index];
+      index++;
+      setTimeout(type, 100); // Adjust the typing speed here (milliseconds)
+    }
+  }
+
+  type();
+});
